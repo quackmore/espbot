0382b31ccdbe7ca863ebcd41df3f2f345e8b73b0
quackmore <quackmore-ff@yahoo.com>
2020-11-19 19:34:49 +0100

    website files revised and webapp created

--- web/sidebar.js
+++ web/sidebar.js
@@ -1,12 +1,16 @@
 // shared.js
 const esp8266 = {
-  "url": "",
-  "cors": false
-  // "url": "http://192.168.1.187",
+  // "url": "",
+  // "cors": false
+  "url": "http://192.168.1.187",
   // "url": "http://192.168.100.100",
   // "url": "http://192.168.10.1",
-  // "cors": true
+  "cors": true
 };
+
+function update_sidebar() {
+  $('#app_settings').hide();
+}
 
 function ajax_error(xhr, status) {
   if (status === "timeout") {
@@ -23,4 +27,9 @@ function ajax_error(xhr, status) {
   setTimeout(function () {
     $('#awaiting').modal('hide');
   }, 1000);
+}
+
+function goto(page) {
+  $('#awaiting').modal('show');
+  $("#page-content").load(page + ".html");
 }
